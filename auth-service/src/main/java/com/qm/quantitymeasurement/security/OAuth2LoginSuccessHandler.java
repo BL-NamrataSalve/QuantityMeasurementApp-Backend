bc8b41e3174b7d09d92b9d1f3c89321ce5aa2c31
BL-NamrataSalve <namrata.salve@bridgelabz.com>
2026-05-28 08:32:45 +0530

    Fix OAuth2 @Value binding for FRONTEND_URL

--- auth-service/src/main/java/com/qm/quantitymeasurement/security/OAuth2LoginSuccessHandler.java
+++ auth-service/src/main/java/com/qm/quantitymeasurement/security/OAuth2LoginSuccessHandler.java
@@ -20,7 +20,7 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
     private final JwtTokenProvider jwtTokenProvider;
     private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
 
-    @org.springframework.beans.factory.annotation.Value("${frontend.url}")
+    @org.springframework.beans.factory.annotation.Value("${FRONTEND_URL}")
     private String frontendUrl;
 
     @Autowired
